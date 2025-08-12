a5e4b6ca4448bf6a4bb4b2d439a8b85d620b51b0
Shemiakin <Anton.Shemiakin@iconplc.com>
2025-08-12 16:49:23 +0300

    Initial commit"

---
+++ app.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(reactable)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  textInput("text1", NULL, placeholder = "Your name"),
+  
+  sliderInput("slider1", "When should we deliver?", 
+              min = as.Date("2020-09-16", "%Y-%m-%d"),
+              max = as.Date("2020-09-23", "%Y-%m-%d"),
+              value = as.Date("2020-09-17", "%Y-%m-%d"),
+              ticks = T),
+  
+  sliderInput("slider2", "slider2",
+              min = 0,
+              max = 100,
+              value = 0,
+              step = 5,
+              animate = animationOptions(interval = 1000,
+                                         loop = F,
+                                         playButton = "Play!")),
+  
+  selectizeInput("selectize1", "Server Select", choices = NULL),
+  
+  selectInput("select1", "SubItem Select", 
+              choices = list(`East Coast` = list("NY", "NJ", "CT"),
+                             `West Coast` = list("WA", "OR", "CA"),
+                             `Midwest` = list("MN"))),
+  
+  verbatimTextOutput("VTout1"),
+  textOutput("Tout1"),
+  
+  verbatimTextOutput("VTout2"),
+  textOutput("Tout2"),
+  
+  plotOutput("plot1", width = "700px", height = "300"),
+  
+  dataTableOutput("dtable1"),
+  
+  reactableOutput("dtable2")
+  
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  updateSelectizeInput(session, "selectize1", 
+                       choices = state.name, 
+                       server = T);
+
+  output$VTout1 <-  renderPrint(summary(mtcars))
+  output$Tout1 <- renderText("Good morning!")
+
+  output$VTout2 <- renderPrint(t.test(1:5, 2:6))
+  output$Tout2 <- renderText(lm(mpg ~ wt, data = mtcars)$coefficients)
+  
+  output$plot1 <- renderPlot(plot(1:5, 
+                                  main = "Scatter Plot by 5 random digits"), 
+                             res = 96)
+  
+  output$dtable1 <- renderDataTable(mtcars, 
+                                    options = list(pageLength = 5, 
+                                                   ordering = F,
+                                                   dom = 'p'))
+  output$dtable2 <- renderReactable({
+    reactable(mtcars)
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
